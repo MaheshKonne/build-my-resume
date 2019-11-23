d582a6334b228a9c20801eeb44e37b3fe0830d96
MaheshKonne <mahesh.konne@thoughtworks.com>
2019-11-23 16:10:09 +0530

    Add styled components and remove css files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import logo from './static/logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import styled, {createGlobalStyle} from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    body {
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+        sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
+`;
+
+const Container = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  color: white;
+`;
+
+const Content = styled.p`
+  font-size: 16px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header>
-          Build My Resume
-      </header>
-      <p>In progress...</p>
-    </div>
+     <Fragment>
+        <GlobalStyles />
+        <Container>
+          <header>
+              Build My Resume
+          </header>
+          <Content>In progress...</Content>
+        </Container>
+     </Fragment>
   );
 }
 
