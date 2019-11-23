6bd05e30c6087a02c60b47d55b7544023f3d7c41
MaheshKonne <mahesh.konne@thoughtworks.com>
2019-11-23 13:50:52 +0530

    Initial react setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './static/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+          Build My Resume
+      </header>
+      <p>In progress...</p>
+    </div>
+  );
+}
+
+export default App;
